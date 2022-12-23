b9d89de8ff4efbd2a514e4e0524670d5a5943cc1
pjrios <98301213+pjrios@users.noreply.github.com>
2022-12-22 20:07:20 -0600

    Update getImages.py

--- Residential_Building_Identification/getImages.py
+++ Residential_Building_Identification/getImages.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# This code appears to be a Python script that processes a GeoJSON file and generates map images using the "allmapsoft" software.
+# This is the main program for this section. It is designed to read a GeoJSON file containing a set of features defined by coordinates, and use those coordinates to retrieve images from Google Maps using the downloader program form "allmapsoft". The program consists of three main parts: and does the following:
 # 
-# Here is a high-level overview of what the code does:
-# 1.Read data from a Json file: It defines a function get_square_bounds that takes a polygon (a list of coordinates) as an argument and returns the coordinates of a square that bounds the polygon. The code opens a GeoJSON file and loads its contents into a variable called data. It iterates through the features in the GeoJSON file and extracts the coordinates of each feature. For each feature, the code calls the get_square_bounds function to calculate the coordinates of a bounding square for the feature.
+# 1. Read data from a Json file: It defines a function get_square_bounds that takes a polygon (a list of coordinates) as an argument and returns the coordinates of a square that bounds the polygon. The code opens a GeoJSON file and loads its contents into a variable called data. It iterates through the features in the GeoJSON file and extracts the coordinates of each feature. For each feature, the code calls the get_square_bounds function to calculate the coordinates of a bounding square for the feature.
 # 
-# 2.Download and combine the images: It generates a command string that calls the "downloader.exe" program from the "allmapsoft" software, passing in the bounding square coordinates and a destination directory for the map image. The code calls the subprocess.run function to execute the command and generate the map image. It then calls the subprocess.run function again to combine the map image into a single file using the "combiner.exe" program from the "allmapsoft" software.
+# 2. Download and combine the images: It generates a command string that calls the "downloader.exe" program from the "allmapsoft" software, passing in the bounding square coordinates and a destination directory for the map image. The code calls the subprocess.run function to execute the command and generate the map image. It then calls the subprocess.run function again to combine the map image into a single file using the "combiner.exe" program from the "allmapsoft" software.
 # 
-# 9.Clean up: It moves the combined map image from the source directory to a destination directory using the shutil.move function. 10. It cleans up by deleting the intermediate files and directories that were created during the processing.
+# 3. Clean up: It moves the combined map image from the source directory to a destination directory using the shutil.move function and then it cleans up by deleting the intermediate files and directories that were created during the processing.
 
 # In[41]:
 
